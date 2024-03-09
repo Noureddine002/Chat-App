f0e39ab9e80d318cc32e34e32f9f9562c908ab74
Noureddine Tanjaoui <noureddine.nt3@gmail.com>
2024-03-09 16:34:52 +0100

    finished desktop and mobile navbar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,10 +1,16 @@
 'use client'
 
 import useRoutes from "@/app/hooks/useRoutes"
-import { useState } from "react";
+import React, { useState } from "react";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
@@ -51,6 +57,26 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav
+        className="
+          mt-4
+          flex
+          flex-col
+          justify-between
+          items-center
+        "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+            cursor-pointer
+            hover:opacity-75
+            transition
+          "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )
